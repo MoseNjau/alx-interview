bf3c6b3770a2dcba9b25c07556f6af0f44275e07
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-07-11 23:55:35 +0300

    operations needed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,15 @@
+#calculating the minimum number of operations needed
 def minOperations(n):
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n /= factor
         factor += 1
-    
+
     return operations
